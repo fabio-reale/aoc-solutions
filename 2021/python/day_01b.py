3e0943baf92c4ae2a46013d5abb9944cb47a9efd
Fábio Reale <ftreale@gmail.com>
2021-12-02 16:08:35 -0300

    encapsulated solution

--- 2021/python/day_01b.py
+++ 2021/python/day_01b.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 
-path = Path(__file__).parents[1] / "inputs" / "day_01.txt"
 
-with open(path) as f:
-    inp = f.read()
+def solve(inp: str, offset: int) -> int:
+    depths = [int(num) for num in inp.split()]
 
-depths = [int(num) for num in inp.split()]
+    return sum((d_prev < d_next for d_prev, d_next in zip(depths, depths[offset:])))
 
-increments = 0
-for d_prev, d_next in zip(depths, depths[3:]):
-    if d_prev < d_next:
-        increments += 1
 
-print(increments)
+if __name__ == "__main__":
+    path = Path(__file__).parents[1] / "inputs" / "day_01.txt"
+
+    with open(path) as f:
+        inp = f.read()
+
+    print(solve(inp, 3))
