835a49ce030d3e8a1a70a8b4147488ed8f47789f
Fábio Reale <ftreale@gmail.com>
2021-12-02 14:41:28 -0300

    added day 01 solutions

---
+++ 2021/python/day_01a.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+path = Path(__file__).parents[1] / "inputs" / "day_01.txt"
+
+with open(path) as f:
+    inp = f.read()
+
+depths = [int(num) for num in inp.split()]
+
+increments = 0
+for d_prev, d_next in zip(depths, depths[1:]):
+    if d_prev < d_next:
+        increments += 1
+
+print(increments)
