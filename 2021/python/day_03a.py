64a1a8a7b146f5beea81671dc4fa98092900a22c
Fábio Reale <ftreale@gmail.com>
2021-12-03 19:14:23 -0300

    refactored part a to use same idea as part b

--- 2021/python/day_03a.py
+++ 2021/python/day_03a.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pathlib import Path
 from collections import defaultdict
 
@@ -5,45 +7,59 @@ from collections import defaultdict
 str_counter = list[defaultdict[str, int]]
 
 
+class Report:
+    def __init__(self, bin_strs: list[str]) -> None:
+        self.bin_strs = bin_strs
+        self.bins: list[int] = []
+        self.max_power = len(bin_strs[0])
+
+    def build_bins(self) -> None:
+        for bin_str in self.bin_strs:
+            num = get_binary_int([c for c in bin_str])
+            self.bins.append(num)
+
+    def calculate_rates(self) -> tuple[int, int]:
+        gammas: list[str] = []
+        epsilons: list[str] = []
+        current_power = self.max_power
+
+        while current_power > 0:
+            current_power -= 1
+            current_split = 2 ** current_power
+            splitted: dict[str, list[int]] = {"1": [], "0": []}
+            for num in self.bins:
+                if num >= current_split:
+                    splitted["1"].append(num - current_split)
+                else:
+                    splitted["0"].append(num)
+
+            if len(splitted["1"]) >= len(splitted["0"]):
+                gammas.append("1")
+                epsilons.append("0")
+            else:
+                gammas.append("0")
+                epsilons.append("1")
+
+            self.bins = splitted["1"] + splitted["0"]
+
+        gamma = get_binary_int(gammas)
+        epsilon = get_binary_int(epsilons)
+
+        return gamma, epsilon
+
+
 def parse_input(inp: str) -> list[str]:
     return inp.split()
 
 
-def count_cols(bins: list[str]) -> str_counter:
-    rows = len(bins)
-    cols = len(bins[0])
-
-    counters: str_counter = [defaultdict(int) for _ in range(cols)]
-    for row in range(rows):
-        for col in range(cols):
-            char = bins[row][col]
-            counters[col][char] += 1
-
-    return counters
+def get_binary_int(bin_str: list[str]) -> int:
+    return int("".join(bin_str), 2)
 
 
-def get_gamma_epsilon(counters: str_counter) -> tuple[int, int]:
-    gammas = []
-    epsilons = []
-
-    for count in counters:
-        if count["1"] > count["0"]:
-            gammas.append("1")
-            epsilons.append("0")
-        else:
-            gammas.append("0")
-            epsilons.append("1")
-
-    gamma = int("".join(gammas), 2)
-    epsilon = int("".join(epsilons), 2)
-
-    return gamma, epsilon
-
-
-def solve(inp: str) -> tuple[int, int]:
-    bins = parse_input(inp)
-    counts = count_cols(bins)
-    gamma, epsilon = get_gamma_epsilon(counts)
+def solve(inp: str) -> int:
+    report = Report(parse_input(inp))
+    report.build_bins()
+    gamma, epsilon = report.calculate_rates()
 
     return gamma * epsilon
 
